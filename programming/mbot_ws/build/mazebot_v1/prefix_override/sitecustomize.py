ea08562b97b93dc4e55258b5554d2b74b6eb1df9
Jakob Kurzmann <kurjam18@htl-kaindorf.at>
2023-04-10 11:26:45 +0200

    Robocup Package

--- programming/mbot_ws/build/mazebot_v1/prefix_override/sitecustomize.py
+++ programming/mbot_ws/build/mazebot_v1/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/kurzmann/mbot_ws/install/mazebot_v1'
+sys.prefix = sys.exec_prefix = '/home/kurzmann/maze_bot/programming/mbot_ws/install/mazebot_v1'
